35384a7b7d8863f85570f6561124f5929fd9210a
Arash Azizi <SirArash@outlook.com>
2020-09-26 13:02:22 +0330

    Initial commit

---
+++ sample/EZCaptchaSample/MainForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace EZCaptchaSample
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbxCaptcha = new System.Windows.Forms.PictureBox();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCaptcha)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxCaptcha
+            // 
+            this.pbxCaptcha.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxCaptcha.Location = new System.Drawing.Point(12, 12);
+            this.pbxCaptcha.Name = "pbxCaptcha";
+            this.pbxCaptcha.Size = new System.Drawing.Size(120, 60);
+            this.pbxCaptcha.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxCaptcha.TabIndex = 6;
+            this.pbxCaptcha.TabStop = false;
+            this.pbxCaptcha.Click += new System.EventHandler(this.pbxCaptcha_Click);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(12, 78);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(120, 20);
+            this.txtAnswer.TabIndex = 10;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(12, 106);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(120, 23);
+            this.btnCheck.TabIndex = 11;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(145, 141);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.pbxCaptcha);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EZCaptcha";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCaptcha)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxCaptcha;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ sample/EZCaptchaSample/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using Captcha;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EZCaptchaSample
+{
+    public partial class MainForm : Form
+    {
+
+        private string GeneratedCaptcha;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CaptchaGenerator()
+        {
+            Random rnd = new Random();
+            string number = rnd.Next(10000, 99999).ToString();
+            EZCaptcha Captcha = new EZCaptcha(number, 100, 39);
+            pbxCaptcha.Image = Captcha.Image;
+            GeneratedCaptcha = Captcha.Text;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            CaptchaGenerator();
+        }
+
+        private void pbxCaptcha_Click(object sender, EventArgs e)
+        {
+            CaptchaGenerator();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            if (txtAnswer.Text == GeneratedCaptcha)
+                MessageBox.Show("Correct answer :)", "EzCaptcha", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("Incorrect answer :(", "EzCaptcha", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
